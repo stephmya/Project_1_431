1d2fe3109828dd8c4c6345eea51ee268a1a44367
Stephanie Myalik <myalikss1@sunypoly.edu>
2024-11-08 21:47:52 -0500

    updates + unused stuff purge

--- calculator/lib/calculator.ml
+++ calculator/lib/calculator.ml
@@ -22,32 +22,28 @@ let matrix_operations mat1 mat2 operator =
 (* Evaluate expression using Owl Matrices, supporting sin cos tan functions *)
 let evaluate_expression expr =
   try
-    (* Check for sin and cos functions *)
-    let func_re = Str.regexp "\\(sin\\|cos\\|tan\\)(\\([0-9.]+\\))" in
+    (* Check for sin, cos, and tan functions *)
+    let func_re = Str.regexp "\\(sin\\|cos\\|tan\\)(\\([0-9]+\\.?[0-9]*\\))" in
     if Str.string_match func_re expr 0 then (
       let func = Str.matched_group 1 expr in
       let angle = Str.matched_group 2 expr |> float_of_string in
       Printf.printf "Matched function: %s, angle: %f\n" func angle;
       let radians = Owl.Mat.of_array [|angle *. (Float.pi /. 180.)|] 1 1 in (* Convert to radians *)
 
-      (* Calculate sine or cosine *)
-      let result =
-        match func with
+      (* Calculate sine, cosine, or tangent *)
+      let result = match func with
         | "sin" -> Owl.Mat.sin radians
         | "cos" -> Owl.Mat.cos radians
         | "tan" -> Owl.Mat.tan radians
-        | _ -> failwith "Unknown trigonometric function"
+        | _ -> failwith "Unknown function"
       in
-
-      (* Extract and return the result as a string *)
       let result_value = Owl.Mat.get result 0 0 in
       Printf.printf "Calculated result: %f\n" result_value;
       string_of_float result_value
-    )
-    (* Check for basic arithmetic expressions *)
-    else
-      let re = Str.regexp "\\([0-9]+\\)\\([-+*/]\\)\\([0-9]+\\)" in
-      if Str.string_match re expr 0 then (
+    ) else (
+      (* Check for basic arithmetic operations *)
+      let expr_re = Str.regexp "\\([0-9]+\\.?[0-9]*\\)\\([+\\-*/]\\)\\([0-9]+\\.?[0-9]*\\)" in
+      if Str.string_match expr_re expr 0 then (
         let operand1 = Str.matched_group 1 expr |> float_of_string in
         let operator = Str.matched_group 2 expr in
         let operand2 = Str.matched_group 3 expr |> float_of_string in
@@ -65,6 +61,7 @@ let evaluate_expression expr =
       )
       else
         "Invalid expression: Unable to parse the input expression."
+    )
   with
   | Failure msg -> "Error: " ^ msg
   | _ -> "An unknown error occurred"
